db9519f671143e13ce4bd340dd4e734f408042fd
bhumic <humic.bruno@gmail.com>
2018-09-14 21:41:13 +0200

    moved the main execution to a separate thread

--- build/msgbox_dll/main.cpp
+++ build/msgbox_dll/main.cpp
@@ -1,11 +1,19 @@
 #include <Windows.h>
 
+DWORD WINAPI run_code(LPVOID param)
+{
+    // TODO: main code for the DLL
+    MessageBoxA(NULL, "Hello from injected DLL!", "DLL injection test", MB_OK);
+    return 1;
+}
+
 BOOL WINAPI DllMain(HMODULE hDll, DWORD dwReason, LPVOID lpReserved)
 {
     switch (dwReason)
     {
     case DLL_PROCESS_ATTACH:
-        MessageBoxA(NULL, "Hello from injected DLL!", "DLL injection test", MB_OK);
+        HANDLE thread = CreateThread(NULL, 0, &run_code, NULL, 0, NULL);
+        CloseHandle(thread);
         break;
     }
 
