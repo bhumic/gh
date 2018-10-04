750806554f7cc59c791b3738cc286af12789f02f
bruno <humic.bruno@gmail.com>
2018-10-04 16:32:49 +0200

    test program is now standalone and import the main static library

--- gh_test/source/main.cpp
+++ gh_test/source/main.cpp
@@ -12,19 +12,19 @@ int main(int argc, char* argv[])
     _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 
     // Test process info data retrieval
-    PROCESS_INFO process_info;
-    get_process_info("ConEmu.exe", process_info);
+    gh::process::PROCESS_INFO process_info;
+    gh::process::get_process_info("ConEmu64.exe", process_info);
     
     // Test if parser for PE is working
-    pe_parser_t* parser = create_pe_parser(process_info);
+    gh::pe::pe_parser_t* parser = gh::pe::create_pe_parser(process_info);
     parser->obtain_handle();
     parser->read_dos_header();
     parser->read_nt_header();
     delete parser;
 
     // Test the injector modules
-    injector_t injector;
-    injector.inject_dll(process_info, "E:\\local_repository\\gh\\build\\msgbox_dll\\Release\\msgbox_dll.dll");
+    gh::injector::injector_t injector;
+    injector.inject_dll(process_info, "E:\\local_repository\\gh\\msgbox_dll\\Release\\msgbox_dll.dll");
     injector.eject_dll(process_info, "msgbox_dll.dll");
 
     CloseHandle(process_info.handle);
